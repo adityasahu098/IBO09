d0ccc7f4b82386ccc4652d52ddc906bc41b82667
Aditya pratap sahu <155720297+adityasahu098@users.noreply.github.com>
2025-04-11 15:45:33 +0530

    Add files via upload

---
+++ CarCareApplication.java
@@ -0,0 +1,13 @@
+package com.exml.abcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCareApplication.class, args);
+	}
+
+}
